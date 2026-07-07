43f76d112e2dfc2b4614f793251b0f023138fcbc
Igor Franca <igrfranca@gmail.com>
2026-07-07 10:26:02 -0300

    Atualizacao projeto web Spring Boot

--- src/main/java/com/produtoapi/controller/ProdutoController.java
+++ src/main/java/com/produtoapi/controller/ProdutoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*") // "*" Permite requisições de qualquer origem
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
